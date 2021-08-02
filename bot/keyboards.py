a04b15bd2e2098c4d605b09993809388bfd3363d
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-08-02 21:41:41 +0300

    Update keyboards.py

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -4,9 +4,12 @@ from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 
-lang = InlineKeyboardMarkup(row_width=2).add(InlineKeyboardButton(text='English', callback_data='English'))
+lang = InlineKeyboardMarkup(row_width=2).add(
+    InlineKeyboardButton(text='English', callback_data='English'))
 lang.add(InlineKeyboardButton(text='Русский', callback_data='Русский'))
 
-start_ru = InlineKeyboardMarkup(row_width=2).add(InlineKeyboardButton(text='Создать пароль', callback_data='Создать пароль'))
+start_ru = InlineKeyboardMarkup(row_width=2).add(
+    InlineKeyboardButton(text='Создать пароль', callback_data='Создать пароль'))
 
-start_eng = InlineKeyboardMarkup(row_width=2).add(InlineKeyboardButton(text='Create Password', callback_data='Create Password'))
+start_eng = InlineKeyboardMarkup(row_width=2).add(InlineKeyboardButton(
+    text='Create Password', callback_data='Create Password'))
